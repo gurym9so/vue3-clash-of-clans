28007ceffdee5c46387a7eef99efaf24ea855ab7
gurym9so <oplo.domo@bk.ru>
2024-02-04 16:39:58 +0300

    create a card and simple character

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,35 @@
 <template>
-  <p>HOME</p>
+  <div class="md-body">
+    <h1>hello home</h1>
+    <p>HOME</p>
+    <div class="card__wrapper" v-for="item in items" :key="item.id">
+      <Card :name="`${item.lvl} lvl`" :title="item.title" />
+    </div>
+  </div>
 </template>
+
+<script>
+import Card from "@/components/UI/Card";
+
+export default {
+  components: { Card },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: "Archer",
+          description: "archer archer archer archer",
+          lvl: 4,
+        },
+        {
+          id: 2,
+          title: "Wizard",
+          description: "wizard wizard wizard wizard",
+          lvl: 3,
+        },
+      ],
+    };
+  },
+};
+</script>
