d9c86c5392aa5f8c233ba2e5fbb75753c32e8e4d
gurym9so <oplo.domo@bk.ru>
2024-02-04 16:09:42 +0300

    changed cfg

--- src/_config.js
+++ src/_config.js
@@ -1,11 +1,3 @@
-export const process = {
-    dev: true,
-}
-
-export const site = {
-    home: process.dev ? 'http://localhost:8080/' : 'http://zalupa.ru'
-}
-
 export const links = [
     {
         title: "Home",
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="navbar">
     <div class="container">
       <div class="navbar-content">
-        <router-link class="navbar-logo" to="/">logo</router-link>
+        <router-link class="navbar-logo" to="/">Clash of clans</router-link>
         <ul class="navbar-list">
           <li class="navbar-item" v-for="link in links" :key="link.alias">
             <router-link class="navbar-link" :to="link.url">{{
