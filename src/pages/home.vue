f51f77a128200f3f2bac0abd08acd0810b5ea015
gurym9so <oplo.domo@bk.ru>
2024-02-06 16:06:12 +0300

    create slider

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="md-body">
-    <div class="card__wrapper" v-for="item in items" :key="item.id">
+  <carousel :settings="settings" :breakpoints="breakpoints">
+    <slide class="card__wrapper" v-for="item in items" :key="item.id">
       <Card
         :name="`${item.lvl} lvl`"
         :title="item.title"
@@ -10,21 +10,61 @@
         <template v-slot:body>
           {{ item.descr }}
         </template>
-        <template v-slot:footer> </template>
+        <template v-slot:footer>
+          <div class="card-stats">
+            <div
+              v-for="(stat, index) in item.info"
+              :key="index"
+              class="one-third"
+            >
+              <div class="stat-value">{{ stat.value }}</div>
+              <div class="stat">{{ stat.title }}</div>
+            </div>
+          </div>
+        </template>
       </Card>
-    </div>
-  </div>
+    </slide>
+
+    <!-- addons -->
+    <template #addons>
+      <navigation />
+    </template>
+  </carousel>
 </template>
 
 <script>
-import Card from "@/components/UI/Card";
 import items from "@/seeders/items.js";
 
+import Card from "@/components/UI/Card";
+
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
+
 export default {
-  components: { Card },
+  components: {
+    Card,
+    Carousel,
+    Slide,
+    Navigation,
+  },
   data() {
     return {
       items: items,
+
+      // corousel
+      settings: {
+        itemsToShow: 2,
+        wrapAround: true,
+        snapAlign: "center",
+      },
+      breakpoints: {
+        300: {
+          itemsToShow: 1,
+        },
+        700: {
+          itemsToShow: 2,
+        },
+      },
     };
   },
 };
